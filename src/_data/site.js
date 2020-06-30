37e4691acfa7e6c1779fbecb76b6d90b86aa9100
Pixelastic <tim@pixelastic.com>
2020-06-30 09:53:57 +0200

    fix(build): Remove useless list of subreddits

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,16 +1,11 @@
-const helper = require('../../lib/main.js');
-
 module.exports = async function () {
   const defaultValues = {
     defaultDescription: 'Auto-updating gallery of pictures from subreddits',
     defaultTitle: 'Reddimg',
     defaultUrl: 'https://reddimg.pixelastic.com',
-    defaultImage: 'opengraph.png',
     defaultAuthor: 'Tim Carry',
     defaultTwitter: 'pixelastic',
   };
-
-  const subreddits = await helper.getLocalSubreddits();
 
   const algolia = {
     appId: process.env.ALGOLIA_APP_ID,
@@ -20,7 +15,6 @@ module.exports = async function () {
 
   return {
     ...defaultValues,
-    subreddits,
     algolia,
   };
 };
